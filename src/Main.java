fa50343a8c3c6bd3a94c98cf7a1948eea45af32d
duongcoder211 <daomanhduong0211@gmail.com>
2026-02-12 18:24:41 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
+
+
+
+//lab5/
+//        │
+//        ├── data/
+//        │   └── data.csv
+//│
+//        ├── src/
+//        │   └── main/
+//        │       └── java/
+//        │           └── ru/
+//        │               └── lab5/
+//        │                   ├── Main.java
+//│                   │
+//        │                   ├── objects/
+//        │                   │   ├── HumanBeing.java
+//│                   │   ├── Coordinates.java
+//│                   │   ├── Car.java
+//│                   │   └── WeaponType.java
+//│                   │
+//        │                   └── utils/
+//        │                       ├── CollectionManager.java
+//│                       ├── FileManager.java
+//│                       ├── InputManager.java
+//│                       ├── HumanBeingReader.java
+//│                       ├── Command.java
+//│                       ├── CommandManager.java
+//│                       │
+//        │                       ├── commands/
+//        │                       │   ├── HelpCommand.java
+//│                       │   ├── InfoCommand.java
+//│                       │   ├── ShowCommand.java
+//│                       │   ├── AddCommand.java
+//│                       │   ├── UpdateIdCommand.java
+//│                       │   ├── RemoveByIdCommand.java
+//│                       │   ├── ClearCommand.java
+//│                       │   ├── SaveCommand.java
+//│                       │   ├── ExecuteScriptCommand.java
+//│                       │   ├── ExitCommand.java
+//│                       │   ├── AddIfMaxCommand.java
+//│                       │   ├── AddIfMinCommand.java
+//│                       │   ├── RemoveGreaterCommand.java
+//│                       │   ├── FilterContainsNameCommand.java
+//│                       │   ├── FilterLessThanMinutesOfWaitingCommand.java
+//│                       │   └── FilterGreaterThanCarCommand.java
+//│                       │
+//        │                       └── exceptions/
+//        │                           ├── InvalidDataException.java
+//│                           ├── CommandExecutionException.java
+//│                           └── ScriptRecursionException.java
+//│
+//        └── README.md
